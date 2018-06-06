f4463b8772b5cb1c72004da6ef339b13e55df9a4
Tina <tparkmedia@gmail.com>
2018-06-06 12:08:20 -0700

    Created the CRUD actions with the quotes controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'quotes/index'
+  get 'quotes/show'
+  get 'quotes/new'
+  get 'quotes/create'
+  get 'quotes/edit'
+  get 'quotes/update'
+  get 'quotes/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
